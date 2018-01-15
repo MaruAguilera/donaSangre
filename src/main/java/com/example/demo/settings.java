1b6b7dfd1a7d94939bdce80130f0f00b5607150a
MaruAguilera <mari.ceci.agui@gmail.com>
2018-01-15 16:13:25 -0300

    cambios en settings por base de datos

--- src/main/java/com/example/demo/settings.java
+++ src/main/java/com/example/demo/settings.java
@@ -1,7 +1,7 @@
 package com.example.demo;
 
 public class settings {
-	public static final String db_url="postgres://ec2-23-23-110-26.compute-1.amazonaws.com:5432/d2ivtubsbquaf8";
-	public static final String db_user="postgres";
-	public static final String db_password="admin";
+	public static final String db_url="DATABASE_URL: postgres://ec2-23-23-110-26.compute-1.amazonaws.com:5432/d2ivtubsbquaf8";
+	public static final String db_user="sjrxkjebbddbcf";
+	public static final String db_password="e2c681a2b163087e183e4f58967ec1e9abb64c412c673020b72b7e88c80f547a";
 }
